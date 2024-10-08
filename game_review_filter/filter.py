686b072a627624f7baeeb323075134aba3c751b2
franciscoduc4 <fduca@fi.uba.ar>
2024-10-07 21:11:27 -0300

    saving games on gamereview filter for post join

---
+++ game_review_filter/filter.py
@@ -0,0 +1,48 @@
+import json
+import logging
+import os
+from common.middleware import Middleware
+from common.utils import split_complex_string
+
+class GameReviewFilter:
+    def __init__(self, input_queues, output_queue, join_key='app_id'):
+        """
+        :param input_queues: Lista de colas de entrada (e.g., ['shooter_games_queue', 'positive_reviews_queue']).
+        :param output_queue: Cola de salida (e.g., 'shooter_games_positive_reviews_queue').
+        :param join_key: Clave para realizar el join (por defecto, 'app_id').
+        """
+        self.middleware = Middleware(input_queues, [], output_queue, 0, self._callback, self._finCallback)
+
+    def saveGameInTxt(self, game):
+        """
+        Guarda el juego en la base de datos.
+        """
+        with open('data/gamesData.txt', 'a') as file:
+            file.write(game + '\n')
+            
+    def start(self):
+        """
+        Inicia el proceso de join.
+        """
+        logging.info("Iniciando GameReviewFilter...")
+        self.middleware.start()
+        logging.info("GameReviewFilter finalizado.")
+
+    def _callback(self, message):
+        """
+        Callback para procesar los mensajes.
+        """
+        try:
+            result = split_complex_string(message)
+            if len(result) > 5: 
+                self.saveGameInTxt(message) 
+      
+        except Exception as e:
+            logging.error(f"Error en GameReviewFilter callback: {e}")
+    
+    def _finCallback(self, message):
+        """
+        Callback para finalizar el proceso.
+        """
+        self.middleware.send(message)
+        logging.info("GameReviewFilter finalizado.")
---
+++ game_review_filter/main.py
@@ -0,0 +1,19 @@
+import json
+import logging
+import os
+import time
+from filter import GameReviewFilter
+import configparser
+
+def main():
+    logging.basicConfig(level=getattr(logging, os.getenv("LOGGING_LEVEL", "DEBUG")),
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    input_queues: dict = json.loads(os.getenv("INPUT_QUEUES")) or {}
+    output_exchanges = json.loads(os.getenv("OUTPUT_EXCHANGES")) or []
+    instance_id = json.loads(os.getenv("INSTANCE_ID") or '1')
+    
+    gameReviewFilter = GameReviewFilter(input_queues, output_exchanges, instance_id)
+    gameReviewFilter.start()
+
+if __name__ == '__main__':
+    main()
