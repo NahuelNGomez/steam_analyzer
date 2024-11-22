283ca5ae46e05319743e269669fb4285a59dda93
franciscoduc4 <fduca@fi.uba.ar>
2024-11-22 20:27:00 -0300

    added persistence to top10 indie counter for query 2

--- top10_indie_counter/counter.py
+++ top10_indie_counter/counter.py
@@ -5,6 +5,7 @@ from common.middleware import Middleware
 from common.utils import split_complex_string
 from common.packet_fin import Fin
 from common.healthcheck import HealthCheckServer
+from common.fault_manager import FaultManager
 
 class Top10IndieCounter:
     def __init__(self, input_queues, output_exchanges, instance_id):
@@ -18,7 +19,21 @@ class Top10IndieCounter:
         )
         #self.game_playtimes = {i: {"nombre": None, "tiempo": None} for i in range(10)}
         self.game_playtimes_by_client = {}
-
+        self.fault_manager = FaultManager(storage_dir="../persistence/")
+        self._init_state()
+        self.last_processed_packet = None
+        
+    def _init_state(self):
+        for key in self.fault_manager.get_keys('top10_indie_counter'):
+            client_id = int(key.split("_")[3])
+            state = self.fault_manager.get(key)
+            if state is not None:
+                data, packet_id = state.rsplit("|", 1)
+                self.game_playtimes_by_client[client_id] = json.loads(data)
+                print(f"Estado cargado desde FaultManager: {self.game_playtimes_by_client[client_id]}", flush=True)
+                self.last_processed_packet = packet_id
+                logging.info('packet id cargado: %s', self.last_processed_packet)
+        
     def get_games(self, client_id):
         """
         Returns the top 10 games dictionary.
@@ -47,11 +62,12 @@ class Top10IndieCounter:
         }
 
 
-    def process_game(self, game):
+    def process_game(self, game, packet_id):
         """
         Processes each message (game) received and adds it to the top 10 list if applicable, based on client_id.
         """
         try:
+            
             client_id = game.client_id  # Obtener el client_id del juego
             game_id = game.id
             name = game.name
@@ -62,7 +78,8 @@ class Top10IndieCounter:
                 self.game_playtimes_by_client[client_id] = {i: {"nombre": None, "tiempo": None} for i in range(10)}
 
             game_playtimes = self.game_playtimes_by_client[client_id]
-
+            aux_game_playtimes = game_playtimes
+            
             menor_puesto = min(
                 (k for k, v in game_playtimes.items() if v["tiempo"] is not None),
                 key=lambda k: game_playtimes[k]["tiempo"],
@@ -76,9 +93,15 @@ class Top10IndieCounter:
 
             if puesto_vacio is not None:
                 game_playtimes[puesto_vacio] = {"nombre": name, "tiempo": playtime}
+            
             elif menor_puesto is not None and playtime > game_playtimes[menor_puesto]["tiempo"]:
                 game_playtimes[menor_puesto] = {"nombre": name, "tiempo": playtime}
 
+            serialized_dict = json.dumps(self.game_playtimes_by_client[client_id])
+            value_to_store = f'{serialized_dict}|{packet_id}'
+            self.fault_manager.update(f'top10_indie_counter_{client_id}', value_to_store)
+            
+            
         except Exception as e:
             logging.error(f"Error in process_game: {e}")
 
@@ -86,11 +109,17 @@ class Top10IndieCounter:
         """
         Callback function to process messages.
         """
-        json_row = json.loads(data)
+        aux = data.strip().split("\n")
+        packet_id = aux[0]
+        logging.debug(f"Received packet with ID: {packet_id}")
+        
+        if packet_id == self.last_processed_packet:
+            logging.info(f"Packet {packet_id} has already been processed, skipping...")
+            return
+        
+        json_row = json.loads(aux[1])
         game = Game.decode(json_row)
-        logging.debug(f"Decoded message: {game}")
-        self.process_game(game)
-        logging.info(f"Processed message: {game}")
+        self.process_game(game, packet_id)
 
     def _eof_callback(self, data):
         """
@@ -102,6 +131,7 @@ class Top10IndieCounter:
             fin_msg = Fin.decode(data)
             client_id = int(fin_msg.client_id)
             self.middleware.send(json.dumps(self.get_games(client_id)))
+            self.fault_manager.delete_key(f"top10_indie_counter_{client_id}")
             
         except Exception as e:
             logging.error(f"Error in _eof_callback: {e}")
